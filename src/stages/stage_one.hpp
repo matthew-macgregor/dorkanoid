eb43235544b790c676ba55b42fdcdc812d0791c8
Matthew MacGregor <m.macgregor@gmail.com>
2015-12-15 07:33:24 -0600

    Fixed whitespace.

--- src/stages/stage_one.hpp
+++ src/stages/stage_one.hpp
@@ -12,26 +12,26 @@
 #include "brick.hpp"
 
 class StageOne : public Stage {
-	
-	public:
-		StageOne();
-		void Init();
-		void Update(const sf::Time& deltaTime);
-		void Draw(sf::RenderWindow& canvas);
-		void Reset();
-		bool IsCompleted();
-	
-	private:
-		Ball ball;
-		Paddle paddle;
-		sf::Font font;
-		sf::Text text;
-		std::vector<Brick> bricks;
-		std::string scoreText;
-		int score = 0;
-		bool isOnARoll = false;
-		
-		void InitBricks();
+    
+    public:
+        StageOne();
+        void Init();
+        void Update(const sf::Time& deltaTime);
+        void Draw(sf::RenderWindow& canvas);
+        void Reset();
+        bool IsCompleted();
+    
+    private:
+        Ball ball;
+        Paddle paddle;
+        sf::Font font;
+        sf::Text text;
+        std::vector<Brick> bricks;
+        std::string scoreText;
+        int score = 0;
+        bool isOnARoll = false;
+        
+        void InitBricks();
 };
 
 #endif
