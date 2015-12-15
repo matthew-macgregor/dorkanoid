46c7c79b1f8daaf218b9ed82d820f73a9f38305e
Matthew MacGregor <m.macgregor@gmail.com>
2015-12-14 20:20:31 -0600

    Initial implementation of python build script.

---
+++ build.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import os, os.path
+import shutil, glob
+
+def cpp_to_obj(src):
+    return os.path.basename(src.replace(".cpp", ".o"))
+
+def find_cpp_files(path):
+    src = []
+    hpp = []
+    
+    for root, dirs, files in os.walk('src'):
+        # print( root, dirs, files )
+        for f in files:
+            if f.endswith('.cpp'):
+                src.append( os.path.join(root, f) )
+            if f.endswith('.hpp'):
+                hpp.append( os.path.join(root, f) )
+            
+    return (src, hpp)
+    
+def is_modified(src, dest):
+    if not os.path.exists(dest):
+        return True
+    
+    srctm = os.path.getmtime(src)
+    objtm = os.path.getmtime(dest)
+    
+    return srctm > objtm
+
+def mkdirectories(obj_dir = 'build/obj', hpp_dir = 'build/include'):    
+    if not os.path.exists(obj_dir):
+        os.makedirs(obj_dir)
+    if not os.path.exists(hpp_dir):
+        os.makedirs(hpp_dir)
+        
+    return (obj_dir, hpp_dir)
+
+def copy_headers(header_list, hpp_dir):
+    print("Copying headers...")
+    for hpp in header_list:
+        dest = os.path.join(hpp_dir, os.path.basename(hpp))
+        if is_modified(hpp, dest):
+            shutil.copyfile(hpp, dest)
+        else:
+            print("Nothing to do for {}".format(hpp))
+
+def build_modules(src_list, obj_dir):
+    print("Building source...")
+    for cpp in src_list:
+        obj = os.path.join(obj_dir, cpp_to_obj(cpp))
+        if is_modified(cpp, obj):
+            gpp = "g++ -Wall -Ibuild/include -g -std=c++11 -c {} -o {} -lsfml-window -lsfml-graphics -lsfml-system -lsfml-audio".format(cpp, obj)
+            print(gpp)
+            os.system(gpp)
+        else:
+            print("Nothing to do for {}".format(cpp))
+
+def link_objects(src_list, obj_dir):
+    objs = []
+    for cpp in src_list:
+        o = os.path.join(obj_dir, cpp_to_obj(cpp))
+        objs.append(o)
+        
+    gpp = "g++ -o  dorkanoid {} -lsfml-window -lsfml-graphics -lsfml-system -lsfml-audio".format(" ".join(objs))
+    print(gpp)
+    os.system(gpp)
+
+if __name__ == '__main__':
+    obj_dir, hpp_dir = mkdirectories()
+    impls, headers = find_cpp_files('src')
+    copy_headers(headers, hpp_dir)
+    build_modules(impls, obj_dir)
+    link_objects(impls, obj_dir)
