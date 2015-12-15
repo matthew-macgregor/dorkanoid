5cd86276a005126ea06528f6a0fc98fe61df1ea1
Matthew MacGregor <m.macgregor@gmail.com>
2015-12-14 23:44:35 -0600

    build.py: Improved relative paths. Improved changes handling.

--- build.py
+++ build.py
@@ -9,12 +9,12 @@ def find_cpp_files(path):
     src = []
     hpp = []
     
-    for root, dirs, files in os.walk('src'):
+    for root, dirs, files in os.walk(path):
         # print( root, dirs, files )
         for f in files:
             if f.endswith('.cpp'):
                 src.append( os.path.join(root, f) )
-            if f.endswith('.hpp'):
+            if f.endswith('.hpp') or f.endswith('.h'):
                 hpp.append( os.path.join(root, f) )
             
     return (src, hpp)
@@ -29,32 +29,44 @@ def is_modified(src, dest):
     return srctm > objtm
 
 def mkdirectories(obj_dir = 'build/obj', hpp_dir = 'build/include'):    
-    if not os.path.exists(obj_dir):
-        os.makedirs(obj_dir)
-    if not os.path.exists(hpp_dir):
-        os.makedirs(hpp_dir)
+    # script_d = os.path.dirname(os.path.realpath(__file__))
+    # od = os.path.join(script_d, obj_dir)
+    # hd = os.path.join(script_d, hpp_dir)
+    od, hd = (obj_dir, hpp_dir)
+    if not os.path.exists(od):
+        os.makedirs(od)
+    if not os.path.exists(hd):
+        os.makedirs(hd)
         
-    return (obj_dir, hpp_dir)
+    return (od, hd)
 
 def copy_headers(header_list, hpp_dir):
-    print("Copying headers...")
+    print("Copying headers to {}...".format(hpp_dir))
+    changes = False
     for hpp in header_list:
         dest = os.path.join(hpp_dir, os.path.basename(hpp))
         if is_modified(hpp, dest):
             shutil.copyfile(hpp, dest)
+            print("Copying {}".format(hpp))
+            changes = True
         else:
             print("Nothing to do for {}".format(hpp))
+    return changes
 
 def build_modules(src_list, obj_dir):
     print("Building source...")
+    changes = False
     for cpp in src_list:
         obj = os.path.join(obj_dir, cpp_to_obj(cpp))
         if is_modified(cpp, obj):
             gpp = "g++ -Wall -Ibuild/include -g -std=c++11 -c {} -o {} -lsfml-window -lsfml-graphics -lsfml-system -lsfml-audio".format(cpp, obj)
             print(gpp)
             os.system(gpp)
+            changes = True
         else:
             print("Nothing to do for {}".format(cpp))
+    
+    return changes
 
 def link_objects(src_list, obj_dir):
     objs = []
@@ -67,8 +79,14 @@ def link_objects(src_list, obj_dir):
     os.system(gpp)
 
 if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    source_dir = os.path.join(script_dir, 'src')
     obj_dir, hpp_dir = mkdirectories()
-    impls, headers = find_cpp_files('src')
-    copy_headers(headers, hpp_dir)
-    build_modules(impls, obj_dir)
-    link_objects(impls, obj_dir)
+    impls, headers = find_cpp_files(source_dir)
+    print(source_dir)
+    changes1 = copy_headers(headers, hpp_dir)
+    changes2 = build_modules(impls, obj_dir)
+    if changes1 and changes2:
+        link_objects(impls, obj_dir)
+    else:
+        print("Nothing to do.")
