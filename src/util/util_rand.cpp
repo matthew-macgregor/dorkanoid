7281cd82cd0e6637c2bb7f088e956d4f027e527b
Matthew MacGregor <m.macgregor@gmail.com>
2015-12-14 20:26:49 -0600

    Reorganized source files.

--- src/util/util_rand.cpp
+++ src/util/util_rand.cpp
@@ -5,7 +5,9 @@
 #include "util_rand.hpp"
 
 float _floatrand(float min, float max) {
+    
     static std::mt19937 rng_engine( (unsigned)time(nullptr) );
     static std::uniform_real_distribution<float> distribution(min, max);
     return distribution(rng_engine);
+    
 }
